9f27f13d3198fb9df4c7a48b7fee678e6fc59a03
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-05 12:30:24 -0700

    Finished assignment

--- task4.py
+++ task4.py
@@ -22,4 +22,11 @@ Enter your name=>Gordon
 You are not a VIP.
 """
 
-VIPNames = ("Guile","Blanka","Christine","Carol","Richard","Daniel","Chun-Li")
+VIPNames = ("Guile","Blanka","Christine","Carol","Richard","Daniel","Chun-Li","Gertrude")
+
+name = input("Enter your name=>")
+
+if name in VIPNames:
+    print(f"Hi, {name}! You are a VIP!")
+else:
+    print("You are not a VIP")
